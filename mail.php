e86aecb86950b2b1ed0ad879482aeb70a320ed11
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2024-03-28 00:06:12 -0300

    Atualização

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contato@henrichsconsultoria.com.br";
+        $recipient = "paulopitarac@gmail.com";
 
         // Set the email subject.
         $subject = "Nova solicitação de Consultoria $name";
